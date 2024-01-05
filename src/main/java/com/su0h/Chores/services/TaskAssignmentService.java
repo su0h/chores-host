3ff6a4662ea6799f49e874da609197f0a7c9f5cf
Andre Ponce <andre_ponce@dlsu.edu.ph>
2024-01-05 21:52:46 +0800

    Implemented unshifting of tasks during double shift days

--- src/main/java/com/su0h/Chores/services/TaskAssignmentService.java
+++ src/main/java/com/su0h/Chores/services/TaskAssignmentService.java
@@ -25,13 +25,16 @@ public class TaskAssignmentService {
     }
 
     public ResponseEntity<List<TaskAssignment>> fetchAllTaskAssignments() {
-
         return ResponseEntity.ok(taskAssignmentRepository.findAll());
     }
 
     // https://www.baeldung.com/spring-scheduled-tasks
     @Scheduled(cron = "* 0 0 * * *") // Runs every 12:00 AM
-    private void shiftTaskAssignments() {
+    private void performScheduledShifting() {
+        this.shiftTaskAssignments();
+    }
+
+    public void shiftTaskAssignments() {
         LocalDate dateToday = LocalDate.now();
         LocalDate lastUpdated = taskAssignmentRepository.getLastModifiedDate();
 
@@ -52,11 +55,11 @@ public class TaskAssignmentService {
             }
 
             // Shift list of tasks
-            tasks.add(0, tasks.remove(tasks.size() - 1));
-
-            // Double shift if today is a weekend or a holiday
+            // (Double shift if today is a weekend or a holiday)
             if (dateService.isDoubleTaskDay(dateToday)) {
-                tasks.add(0, tasks.remove(tasks.size() - 1));
+                this.shiftTasks(tasks, 2, true);
+            } else {
+                this.shiftTasks(tasks, 1, true);
             }
 
             // Update task assignments
@@ -68,5 +71,55 @@ public class TaskAssignmentService {
             // Save updated task assignments
             taskAssignmentRepository.saveAll(taskAssignments);
         }
+    }
+
+    // TODO: Try to merge with shiftTaskAssignments() (code duplication)
+    public ResponseEntity<String> unshiftTaskAssignments() {
+        LocalDate dateToday = LocalDate.now();
+        LocalDate lastUpdated = taskAssignmentRepository.getLastModifiedDate();
+
+        // Unshift only if (1) db has been shifted already AND (2) today is a double task day
+        if (dateToday.isEqual(lastUpdated) && dateService.isDoubleTaskDay(LocalDate.now())) {
+            // Save all task assignments
+            List<TaskAssignment> taskAssignments = taskAssignmentRepository.findAll();
+
+            // Empty table of task assignments
+            // Note: ensure that cascade is not set to ALL
+            taskAssignmentRepository.deleteAll();
+
+            // Retrieve tasks stored in task assignments
+            ArrayList<Task> tasks = new ArrayList<>();
+            for (TaskAssignment taskAssignment : taskAssignments) {
+                tasks.add(taskAssignment.getTask());
+            }
+
+            // Unshift list of tasks
+            this.shiftTasks(tasks, 1, false);
+
+            // Update task assignments
+            for (int i = 0; i < taskAssignments.size(); i++) {
+                taskAssignments.get(i).setTask(tasks.get(i));
+                taskAssignments.get(i).setLastModified(dateToday);
+            }
+
+            // Save updated task assignments
+            taskAssignmentRepository.saveAll(taskAssignments);
+
+            return ResponseEntity.ok("Unshifted successfully!");
+        }
+
+        if (!dateToday.isEqual(lastUpdated))
+            return ResponseEntity.ok("Unable to un-shift. Tasks for today are not shifted yet!");
+        else
+            return ResponseEntity.ok("Unable to un-shift. Today's not a double task day!");
+    }
+
+    private void shiftTasks(ArrayList<Task> tasks, int shiftAmount, boolean shiftRight) {
+        for (int i = 0; i < shiftAmount; i++)
+            if (shiftRight) {
+                tasks.add(0, tasks.remove(tasks.size() - 1));
+            } else {
+                tasks.add(tasks.size() - 1, tasks.remove(0));
+            }
     }
 }
