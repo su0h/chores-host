35142ae412195786dd6a88624273145af2c67e60
Andre Ponce <ndr.hbk@gmail.com>
2024-06-16 20:42:28 +0800

    Added 2024 holidays initialization

--- src/main/java/com/su0h/Chores/ChoresApplication.java
+++ src/main/java/com/su0h/Chores/ChoresApplication.java
@@ -9,7 +9,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
@@ -27,7 +29,8 @@ public class ChoresApplication {
 //			PersonRepository personRepository,
 //			TaskRepository taskRepository,
 //			TaskAssignmentRepository taskAssignmentRepository,
-//			MetadataRepository metadataRepository
+//			MetadataRepository metadataRepository,
+//			HolidayRepository holidayRepository
 //	) {
 //		return (args) -> {
 //			// Note: use this in conjunction with spring.jpa.hibernate.ddl-auto=create-drop in application.properties
@@ -42,10 +45,37 @@ public class ChoresApplication {
 //			List<Person> personList = personRepository.findAll();
 //			List<Task> taskList = taskRepository.findAll();
 //
-//			for(int i = 0; i < personList.size(); i++)
+//			for (int i = 0; i < personList.size(); i++)
 //				taskAssignmentRepository.save(new TaskAssignment(personList.get(i), taskList.get(i)));
 //
 //			metadataRepository.save(new Metadata(Metadata.Key.LAST_MODIFIED, LocalDateTime.now().toString()));
+//
+//			List<Holiday> holidayList = new ArrayList<>();
+//
+//			// From: https://www.globe.com.ph/go/travel-food/article/official-list-holidays-philippines
+//			holidayList.add(new Holiday("New Year's Day", LocalDate.of(2024, 1, 1)));
+//			holidayList.add(new Holiday("Maundy Thursday", LocalDate.of(2024, 3, 28)));
+//			holidayList.add(new Holiday("Good Friday", LocalDate.of(2024, 3, 29)));
+//			holidayList.add(new Holiday("Day of Valor", LocalDate.of(2024, 4, 9)));
+//			holidayList.add(new Holiday("Eid'l Fitr", LocalDate.of(2024, 4, 10)));
+//			holidayList.add(new Holiday("Labor Day", LocalDate.of(2024, 5, 1)));
+//			holidayList.add(new Holiday("Independence Day", LocalDate.of(2024, 6, 12)));
+//			holidayList.add(new Holiday("Eid'l Adha", LocalDate.of(2024, 6, 17)));
+//			holidayList.add(new Holiday("National Heroes Day", LocalDate.of(2024, 8, 26)));
+//			holidayList.add(new Holiday("Bonifacio Day", LocalDate.of(2024, 11, 30)));
+//			holidayList.add(new Holiday("Christmas Day", LocalDate.of(2024, 12, 25)));
+//			holidayList.add(new Holiday("Rizal Day", LocalDate.of(2024, 12, 30)));
+//			holidayList.add(new Holiday("Chinese New Year", LocalDate.of(2024, 2, 9)));
+//			holidayList.add(new Holiday("Black Saturday", LocalDate.of(2024, 3, 30)));
+//			holidayList.add(new Holiday("Ninoy Aquino Day", LocalDate.of(2024, 8, 21)));
+//			holidayList.add(new Holiday("All Saints' Day", LocalDate.of(2024, 11, 1)));
+//			holidayList.add(new Holiday("All Souls' Day", LocalDate.of(2024, 11, 2)));
+//			holidayList.add(new Holiday("Feast of the Immaculate Conception of Mary", LocalDate.of(2024, 12, 8)));
+//			holidayList.add(new Holiday("Christmas Eve", LocalDate.of(2024, 12, 24)));
+//			holidayList.add(new Holiday("Last Day of the Year", LocalDate.of(2024, 12, 31)));
+//
+//			for (Holiday holiday : holidayList)
+//				holidayRepository.save(holiday);
 //
 //			logger.info("Users found with findAll():");
 //			logger.info("---------------------------");
