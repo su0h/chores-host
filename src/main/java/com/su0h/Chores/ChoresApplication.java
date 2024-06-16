3d38f54601a0082866224e10584df5c310ea2d78
Andre Ponce <ndr.hbk@gmail.com>
2024-06-16 19:46:19 +0800

    Updated the demo() method to be a disabled db init method

--- src/main/java/com/su0h/Chores/ChoresApplication.java
+++ src/main/java/com/su0h/Chores/ChoresApplication.java
@@ -1,9 +1,16 @@
 package com.su0h.Chores;
 
+import com.su0h.Chores.entities.*;
+import com.su0h.Chores.repositories.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootApplication
 public class ChoresApplication {
@@ -16,36 +23,49 @@ public class ChoresApplication {
 	}
 
 //	@Bean
-//	public CommandLineRunner demo(PersonRepository personRepository, TaskRepository taskRepository) {
+//	public CommandLineRunner initializeDatabase(
+//			PersonRepository personRepository,
+//			TaskRepository taskRepository,
+//			TaskAssignmentRepository taskAssignmentRepository,
+//			MetadataRepository metadataRepository
+//	) {
 //		return (args) -> {
-//			personRepository.save(new Person("Alice"));
+//			// Note: use this in conjunction with spring.jpa.hibernate.ddl-auto=create-drop in application.properties
+//			personRepository.save(new Person("Anne"));
 //			personRepository.save(new Person("Bob"));
 //			personRepository.save(new Person("Jorhe"));
 //
-//			taskRepository.save(new Task("Wash Dishes"));
+//			taskRepository.save(new Task("Wash Dish"));
 //			taskRepository.save(new Task("Prepare Table"));
-//			taskRepository.save(new Task("Clean Table"));
+//			taskRepository.save(new Task("Clean Up Table"));
 //
-//			log.info("Users found with findAll():");
-//			log.info("---------------------------");
+//			List<Person> personList = personRepository.findAll();
+//			List<Task> taskList = taskRepository.findAll();
+//
+//			for(int i = 0; i < personList.size(); i++)
+//				taskAssignmentRepository.save(new TaskAssignment(personList.get(i), taskList.get(i)));
+//
+//			metadataRepository.save(new Metadata(Metadata.Key.LAST_MODIFIED, LocalDateTime.now().toString()));
+//
+//			logger.info("Users found with findAll():");
+//			logger.info("---------------------------");
 //			personRepository.findAll().forEach(person -> {
-//				log.info(person.toString());
+//				logger.info(person.toString());
 //			});
-//			log.info("");
+//			logger.info("");
 //
 //			Person person = personRepository.findById(1L);
-//			log.info("Customer found with findById(1L):");
-//			log.info("--------------------------------");
-//			log.info(person.toString());
-//			log.info("");
+//			logger.info("Customer found with findById(1L):");
+//			logger.info("--------------------------------");
+//			logger.info(person.toString());
+//			logger.info("");
 //
-//			log.info("Customer found with findByName('Jorhe'):");
-//			log.info("---------------------------------------");
-//			personRepository.findByName("Jorhe").forEach(jorhe -> {
-//				log.info(jorhe.toString());
+//			logger.info("Customer found with findByName('Jorhe'):");
+//			logger.info("---------------------------------------");
+//			personRepository.findByName("Andre").forEach(jorhe -> {
+//				logger.info(jorhe.toString());
 //			});
-//			log.info("");
+//			logger.info("");
 //		};
 //	}
-
 }
